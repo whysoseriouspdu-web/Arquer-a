a420d8843220e2e02c98a6b137d44974e40e816d
whysoseriouspdu-web <whysoseriouspdu@gmail.com>
2026-09-17 09:03:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "arqueria-v10-3";
+const CACHE = "arqueria-v10-4";
 const FILES = [
   "./", "./index.html", "./manifest.webmanifest",
   "./jsqr.min.js", "./logo-club.png", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png"
